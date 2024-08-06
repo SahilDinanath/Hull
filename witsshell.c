bf0ed800b2fbe16fb8e4e486f8094753a9ab6413
Sahil Dinanath <sahildinanath@gmail.com>
2024-08-06 10:46:50 +0200

    initial project setup

---
+++ witsshell.c
@@ -0,0 +1,10 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int MainArgc, char *MainArgv[]) { return (0); }
