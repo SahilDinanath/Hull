3b3a2709153bda3130b25607fc497391d98cbf29
Sahil Dinanath <sahildinanath@gmail.com>
2024-08-06 11:46:23 +0200

    added basic structure of interactive mode and batch mode

--- witsshell.c
+++ witsshell.c
@@ -7,4 +7,37 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(int MainArgc, char *MainArgv[]) { return (0); }
+int main(int MainArgc, char *MainArgv[]) {
+  // stdin: 0, stdout: 2, stderr: 3
+  FILE *stream = stdin;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t nread;
+
+  if (MainArgc > 2) {
+    fprintf(stderr, "Expected 1 argument, recieved %d", MainArgc);
+    exit(EXIT_FAILURE);
+  }
+
+  if (MainArgc == 2) {
+    if ((stream = fopen(MainArgv[1], "r")) == NULL) {
+      fprintf(stderr, "File %s not found.", MainArgv[1]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  printf("witsshell> ");
+  while ((nread = getline(&line, &len, stream)) != -1) {
+    // fwrite(line, nread, 1, stdout);
+
+    // any string retrieved with getline ends with \n
+    if (strcmp(line, "exit\n") == 0) {
+      exit(EXIT_SUCCESS);
+    }
+    printf("witsshell> ");
+  }
+
+  free(line);
+  fclose(stream);
+  exit(EXIT_SUCCESS);
+}
